765bb88fe4d106cf1f26e1cd769fb125ff78cbda
wyatt4y <lygwhy@vip.qq.com>
2022-04-14 00:17:23 +0100

    Update ImageInputPage

--- ImageInputPage.js
+++ ImageInputPage.js
@@ -6,11 +6,22 @@ import {
   ScrollView,
   Image,
   TouchableOpacity,
+  Linking
 } from 'react-native';
 import TextInputPage from './TextInputPage';
 import MainPage from './MainPage';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+
 
 class ImageInputPage extends React.Component{
+
+    onSuccess = e => {
+        // Linking.openURL(e.data).catch(err =>
+        //   console.error('An error occured', err)
+          alert(e.data)
+        // );
+      };
 
     constructor(props){
         super(props)
@@ -34,30 +45,22 @@ class ImageInputPage extends React.Component{
             return(<MainPage></MainPage>)
         } else {
             return(
-                <View style={{backgroundColor:'#222222'}}>
-                    <View style={[styles.flexs, {height: 80 }]}>
-                        <TouchableOpacity onPress={this.backPress}>
-                            <Image style={[styles.imageBack]} source={require('./src/img/png/返回.png')}></Image>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity>
-                            <Text style={[styles.textProblems]}>any problems?</Text>
-                        </TouchableOpacity>
-
-
-                        <TouchableOpacity>
-                            <Image style={[styles.imageMore]} source={require('./src/img/png/更多.png')}></Image>
-                        </TouchableOpacity>
-                    </View>
-
-                    <View style={[styles.viewCameraFunction]}>
-                        <Text>======Camera Function Here======</Text>
-                    </View>
-
-                    <View style={[styles.viewReminder]}>
-                        <Text style={[styles.textReminder]}>Please scan the code on the food</Text>
-                    </View>
-                </View>
+                <QRCodeScanner
+        onRead={this.onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.auto}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
                 
             )
         }
@@ -65,62 +68,23 @@ class ImageInputPage extends React.Component{
 }
 
 const styles = StyleSheet.create({
-
-    flexs: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
+    centerText: {
+      flex: 1,
+      fontSize: 18,
+      padding: 32,
+      color: '#777'
     },
-
-    imageBack: {
-        left: 20, 
-        width: 25, 
-        height: 25, 
-        resizeMode: 'contain', 
-        marginTop: 10,
-        left:20
+    textBold: {
+      fontWeight: '500',
+      color: '#000'
     },
-
-    textProblems: {
-        textAlign: 'center',
-        alignItems:'center',
-        right: -3,
-        marginTop: 10,
-        color: '#ffffff',
-        textDecorationLine: 'underline'
+    buttonText: {
+      fontSize: 21,
+      color: 'rgb(0,122,255)'
     },
-    
-    imageMore: {
-        left: 20, 
-        width: 25, 
-        height: 25, 
-        resizeMode: 'contain', 
-        marginTop: 10,
-        left:-12
-    },
-
-    viewCameraFunction: {
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        backgroundColor: '#e4e0d8', 
-        height: 550
-    },
-
-    viewReminder: {
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        backgroundColor: '#e4e0d8', 
-        height: 100,
-        backgroundColor:'#222222'
-    },
-
-    textReminder:{
-        fontFamily: 'Arial',
-        color:'#58c0a9',
-        fontSize: 18, 
+    buttonTouchable: {
+      padding: 16
     }
-    
-})
+  });
 
 export default ImageInputPage;
