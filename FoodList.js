c08d48f7324ca72d5bd994d8a16d88b94e295454
SiebenElf <sgyhe14@liverpool.ac.uk>
2022-04-13 22:35:56 +0100

    4/13

--- FoodList.js
+++ FoodList.js
@@ -17,13 +17,71 @@ function FoodList(props) {
     } else {
         var newList = list.push(props.line)
     }
-    return (
-        <View>
-            <Text>===Write your foodlist code here===</Text>
-            <Text>{newList}</Text>
-        </View>
-    )
-}
+
+    
+        return (
+                
+            <View>
+              <View style={[styles.flexs, { height: 80}]}>
+              <Image style={{ left: 50, width: 50, height: 50, resizeMode: 'contain'}} source={require('./src/img/png/返回-黑.png')}></Image>
+              <Text style={[styles.TitleName]}>name</Text>
+              <Text style={[styles.Number]}>10</Text>
+              <Text style={[styles.Days]}>days</Text>
+              <Image style={{ left: -60, width: 300, height: 50, marginTop: 40, resizeMode: 'contain'}} source={require('./src/img/png/进度条.png')}></Image>
+              </View>
+    
+            </View>
+            
+             )    
+
+    }
+        
+
+
+const styles = StyleSheet.create({
+
+    flexs: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    TitleName: {
+        left: 68, 
+        fontWeight: 'bold', 
+        textAlign: 'center', 
+        fontSize: 20, 
+        color: '#333333', 
+        marginTop: 20,
+        height: 70
+    },
+
+    Number:{
+        left: 230, 
+        fontWeight: 'bold', 
+        textAlign: 'center', 
+        fontSize: 30, 
+        color: '#333333', 
+        marginTop: 20,
+        height: 70
+
+    },
+
+    Days: {
+        left: 240, 
+        fontWeight: 'bold', 
+        textAlign: 'center', 
+        fontSize: 20, 
+        color: '#233333', 
+        marginTop: 10,
+        height: 50
+
+    }
+
+    
+    
+})
 
 
 export default FoodList;
