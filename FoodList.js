d1b81269e2dc2e4c3dd40905af018d2efac02f00
YankFitzgerald <yank.fitzgerald@gmail.com>
2022-04-22 03:42:54 +0100

    Show details window
    Tap an item will show a details window now

--- FoodList.js
+++ FoodList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     StyleSheet,
     View,
@@ -6,7 +6,9 @@ import {
     Image,
     TouchableOpacity,
     Alert,
-    Button
+    Button,
+    Modal,
+    Pressable
 } from 'react-native';
 import { MainContext } from './App';
 import Swipeout from 'react-native-swipeout';
@@ -19,8 +21,21 @@ function FoodList() {
     let currentDate = new Date();
 
     if (mc.state.foodList.length > 0) {
+        
+        // Handle the visible of details page
+        let visible = [];
+        for (let j = 0; j < mc.state.foodList.length; j++) {visible.push(false)}
+        const [detailsVisible, setDetailsVisible] = useState({visibleList:visible});
+        const showDetails = (i) => {
+            visible[i] = true;
+            setDetailsVisible({visibleList:visible});
+        }
+        const hideDetails = (i) => {
+            visible[i] = false;
+            setDetailsVisible({visibleList:visible});
+        }
+
         for (let i = 0; i < mc.state.foodList.length; i++) {
-            // Best before date of item i
             let foodInfo = []
             let bbDate = new Date(mc.state.foodList[i].bbDate);
             var differenceTime = bbDate.getTime() - currentDate.getTime();
@@ -56,7 +71,8 @@ function FoodList() {
             let item = (
                 <View key={[i, parseInt(differenceDay)]}>
                     <Swipeout right={swipeoutBtns} style = {{backgroundColor: '#ffffff'}}>
-                    <TouchableOpacity onPress={() => { Alert.alert(foodInfoList[i][0], "Best before date: \n" + foodInfoList[i][1] + "\nNotes:\n" + foodInfoList[i][2]) }}>
+                    {/* <TouchableOpacity onPress={() => { Alert.alert(foodInfoList[i][0], "Best before date: \n" + foodInfoList[i][1] + "\nNotes:\n" + foodInfoList[i][2]) }}> */}
+                    <TouchableOpacity onPress={() => {showDetails(i)}}>
                         <View style={[styles.flexs, { height: 80 }]}>
                             <Image style={[styles.foodImage]} source={imagePreview}></Image>
                             <Text style={[styles.TitleName]}>{foodNamePreview}</Text>
@@ -65,7 +81,35 @@ function FoodList() {
                         </View>
                     </TouchableOpacity>
                     </Swipeout>
+
+                    {/* Show details of food */}
+                    <Modal
+                        animationType="fade"
+                        transparent={true}
+                        visible={detailsVisible.visibleList[i]}
+                        onRequestClose = {()=> {
+                            {hideDetails(i)};
+                        }}
+                    >
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalView}>
+                                <Image style={[styles.detailsImage]} source={imagePreview}></Image>
+                                <Text style={styles.detailsNameText}>{foodInfoList[i][0]}</Text>
+                                <Text style={styles.detailsBBDText}>Best before date: {foodInfoList[i][1]}</Text>
+                                <Text style={styles.detailsNoteText}>Notes: {foodInfoList[i][2]}</Text>
+
+                                <Pressable
+                                   style={[styles.button, styles.buttonClose]}
+                                   onPress={() => {hideDetails(i)}}
+                                >
+                                    <Text style={styles.textStyle}>OK</Text>
+                                </Pressable>
+                            </View>
+                        </View>
+                    </Modal>
+
                 </View>
+                
             )
             listArr.push(item)
         } 
@@ -189,7 +233,64 @@ const styles = StyleSheet.create({
     //     borderTopLeftRadius: px2dp(2000),
     //     zIndex: 8
     // },
-
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+        backgroundColor: "#2196F3",
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    detailsImage: {
+        display: 'flex',
+        alignItems: 'center',
+        width: 150, height: 150, resizeMode: 'contain'
+    },
+    detailsNameText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontWeight: 'bold',
+        fontSize: 25,
+    },
+    detailsBBDText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontSize: 15,
+    },
+    detailsNoteText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontSize: 15,
+    },
 
 })
 
